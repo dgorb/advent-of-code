ddc11c3448322c449705f2e388e485f4faa15e97
Denys Gorbach <denis.gorbach@gmail.com>
2023-12-05 00:59:10 +0100

    advent of code 2023, day 01

---
+++ 2023/01/01.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	lines := fileToStringSlice("input.txt")
+
+	part1 := calculateValue(lines)
+	part2 := calculateValue(replaceStringsWithDigits(lines))
+
+	fmt.Println("Part 1:", part1)
+	fmt.Println("Part 2:", part2)
+}
+
+func calculateValue(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		ints := regexp.MustCompile(`\d`).FindAllString(line, -1)
+		if len(ints) > 0 {
+			val, _ := strconv.Atoi(ints[0] + ints[len(ints)-1])
+			sum += val
+		}
+	}
+	return sum
+}
+
+func replaceStringsWithDigits(lines []string) []string {
+	replacementTable := map[string]string{
+		"one":   "o1e",
+		"two":   "t2o",
+		"three": "t3e",
+		"four":  "f4",
+		"five":  "f5e",
+		"six":   "s6",
+		"seven": "s7n",
+		"eight": "e8t",
+		"nine":  "n9e",
+	}
+
+	replacedLines := []string{}
+	for _, line := range lines {
+		replacedLine := line
+		for k, v := range replacementTable {
+			replacedLine = regexp.MustCompile(k).ReplaceAllString(replacedLine, v)
+		}
+		replacedLines = append(replacedLines, replacedLine)
+	}
+	return replacedLines
+}
+
+// TODO: Move to a utils package
+func fileToStringSlice(path string) []string {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	lines := []string{}
+	lines = append(lines, strings.Split(string(content), "\n")...)
+
+	return lines
+}
---
+++ 2023/go.mod
@@ -0,0 +1,3 @@
+module github.com/dgorb/advent-of-code/2023
+
+go 1.21.4
